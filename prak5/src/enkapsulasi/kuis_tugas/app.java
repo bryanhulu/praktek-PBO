28e7186de9c6696256e9b9860462900882175f58
bryanjr342-creator <bryanjr342@gmail.com>
2025-10-20 14:53:21 +0700

    Prak PBO

--- prak5/src/enkapsulasi/kuis_tugas/app.java
+++ prak5/src/enkapsulasi/kuis_tugas/app.java
@@ -1,14 +1,29 @@
 package enkapsulasi.kuis_tugas;
 
+import java.util.Scanner;
+
 public class app {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         kucing kucing1 = new kucing();
-        kucing1.setNama("Kucing");
-        kucing1.setUmur("2 tahun");
-        kucing1.setAktivitas("Sedang bemain");
 
-        System.out.println(kucing1.getNama() + kucing1.getUmur() + kucing1.getAktivitas());
+        System.out.print("Masukkan Nama Hewan : ");
+        kucing1.setNama(scanner.nextLine());
+
+        System.out.print("Masukkan Type Hewan : ");
+        kucing1.setType(scanner.nextLine());
+
+        System.out.print("Masukkan Umur Hewan : ");
+        kucing1.setUmur(scanner.nextLine());
+
+        System.out.println("\n=== Data Hewan ===");
+        System.out.println("Nama  : " + kucing1.getNama());
+        System.out.println("Jenis : " + kucing1.getType());
+        System.out.println("Umur  : " + kucing1.getUmur() + " tahun\n");
+
+        kucing1.playing();
+        kucing1.sleeping();
+
+        scanner.close();
     }
-    
 }
--- prak5/src/enkapsulasi/kuis_tugas/kucing.java
+++ prak5/src/enkapsulasi/kuis_tugas/kucing.java
@@ -3,6 +3,7 @@ package enkapsulasi.kuis_tugas;
 public class kucing {
 
     private String nama;
+    private String type;
     private String umur;
     private String aktivitas;
     
@@ -12,12 +13,31 @@ public class kucing {
     public void setNama(String nama) {
         this.nama = nama;
     }
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public String getUmur() {
         return umur;
     }
-    public void setUmur(String umur) {
-        this.umur = umur;
+public void setUmur(String umurInput) {
+    try {
+        int umur = Integer.parseInt(umurInput);
+        if (umur <= 0) {
+            System.out.println("Umur harus lebih dari 0");
+            return;
+        }
+        this.umur = umurInput;
+    } catch (NumberFormatException e) {
+        System.out.println("Umur harus berupa angka");
     }
+
+    }
+
     public String getAktivitas() {
         return aktivitas;
     }
@@ -25,4 +45,10 @@ public class kucing {
         this.aktivitas = aktivitas;
     }
     
+    public void playing() {
+        System.out.println(nama + " yang berumur " + umur + " tahun Sedang bermain");
+    }
+    public void sleeping() {
+        System.out.println(nama + " yang berumur " + umur + " tahun sedang tidur!");
+    }
 }
